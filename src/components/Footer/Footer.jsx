801171f17297cc360b73aad472421150fa2d1359
newelhan <ne.welhan@gmail.com>
2024-09-30 13:08:03 +0700

    Fixed the contact link in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
           <p>&copy; 2024 newelhan. All Rights Reserved.</p>
           <div className="social-links">
             <a href="https://github.com/newelhan">GitHub</a>
-            <a href="../contact/contact.html">Contact</a>
+            <a href="../contact">Contact</a>
           </div>
         </div>
       </div>
